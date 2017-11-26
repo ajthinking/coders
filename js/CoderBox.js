fbb03b1b5527fc1c9fb7f7c0bbe8b0c61fd46e44
Anders Jürisoo <jurisoo@hotmail.com>
2017-11-26 22:31:43 +0100

    added bot speach

--- js/CoderBox.js
+++ js/CoderBox.js
@@ -10,15 +10,22 @@ var CoderBox = React.createClass({
 
         var url = `https://api.github.com/users/${username}/repos`;
         $.get(url, function(repos) {
-            var coders = this.state.coders.concat({ username, randomRepo: repos[Math.floor((Math.random() * repos.length))].name });
-            this.setState({ coders });    
+            var selectedRepoIndex = Math.floor((Math.random() * repos.length));
+            var coders = this.state.coders.concat({ username, randomRepo: repos[selectedRepoIndex].name });
+            this.setState({ coders });
+            
+            var msg = new SpeechSynthesisUtterance();            
+            msg.text = `Wow, ${username} seems to be a really good boy, he actually created ${repos[selectedRepoIndex].name}. We should problably hire him.`;
+            window.speechSynthesis.speak(msg);
         }.bind(this));        
     },
 
     render: function() {
         return (
             <div className="container">
-                <h1>Coders</h1>
+                <h3>Reqruitment bot</h3>
+                <img src="https://giant.gfycat.com/ShrillUnfortunateCod.gif"/>
+                <h4>your expert on github reviews.</h4>
                 { this.state.coders.map(function(coder) {
                     return (<Coder username={coder.username} randomRepo={coder.randomRepo} />);
                 })}
